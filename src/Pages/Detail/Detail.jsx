d8b098f1ace058df4f30f000fd9d6e619ef589be
ikmat-rabib <ikmat.rbb@gmail.com>
2023-09-26 11:45:06 +0600

    added toast and created local storage

--- src/Pages/Detail/Detail.jsx
+++ src/Pages/Detail/Detail.jsx
@@ -1,10 +1,12 @@
 import { useLoaderData, useParams } from "react-router-dom";
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { saveDonation } from "../../utility/localStorage";
 
 const Detail = () => {
 
     const cards = useLoaderData();
-    const { id,  title, text_and_button_bg_color, description } = useParams();
+    const { id } = useParams();
 
     const card = cards.find(card => card.id == id)
 
@@ -13,20 +15,26 @@ const Detail = () => {
         backgroundColor: card.text_and_button_bg_color
     };
 
-    console.log(card);
+    // console.log(card);
+
+    const handleAddedDonation = () => {
+        saveDonation(id);
+        toast("You have already Donated this !");
+    }
 
     return (
         <div className="max-w-[1320px] mx-auto my-10">
             <div className="mb-8 relative ">
                 <figure><img className="w-full rounded-lg object-cover" src={card.picture} alt="" /></figure>
                 <div className="bg-[#0B0B0B80] absolute bottom-0 w-full py-9 pl-9 rounded-b-lg">
-                    <button className="px-7 py-4 font-semibold rounded-lg" style={buttonStyle}>Donate $290</button>
+                    <button onClick={handleAddedDonation} className="px-7 py-4 font-semibold rounded-lg" style={buttonStyle}>Donate $290</button>
                 </div>
             </div>
             <div className="my-16">
                 <h2 className="text-5xl font-bold ">{card.title}</h2>
                 <p className="my-6">{card.description}</p>
             </div>
+            <ToastContainer />
         </div>
     );
 };
---
+++ src/utility/localStorage.js
@@ -0,0 +1,20 @@
+import { stringify } from "postcss";
+
+const getStoredDonation = () => {
+    const storedDonation = localStorage.getItem('donations');
+    if (storedDonation) {
+        return JSON.parse(storedDonation);
+    }
+    return [];
+}
+
+const saveDonation = id => {
+    const storedDonations = getStoredDonation();
+    const exist = storedDonations.find(donationId => donationId == id)
+    if (!exist) {
+        storedDonations.push(id);
+        localStorage.setItem('donations',JSON.stringify(storedDonations))
+    }
+}
+
+export { getStoredDonation, saveDonation }
